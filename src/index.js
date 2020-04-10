4f51baf4f626e006e335d02ff594c4057953ef36
Erika Miwa <erikamiwa612@gmail.com>
2020-04-10 14:00:30 -0700

    add customStore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-function sayHello() {
+import store from './customStore';
+import * as actions from './actions';
 
-}
+store.subscribe(() => {
+  console.log("Store changed!")
+})
+
+store.dispatch(actions.bugAdded("Bug 1"))
+
+console.log(store.getState());
