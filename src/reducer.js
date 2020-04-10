33a6d4b574457bc64bc7f85e36f43dba3afbd832
Erika Miwa <erikamiwa612@gmail.com>
2020-04-09 23:57:12 -0700

    implement reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,21 @@
-function reducer(state, action) {
+let lastId = 0;
 
+function reducer(state = [], action) {
+  switch (action.type) {
+    case "bugAdded":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolve: false
+        }
+      ];
+  
+    case "bugRemoved":
+      return state.filter(bug => bug.id !== action.payload.id);
+    
+    default:
+      return state;
+  }
 }
