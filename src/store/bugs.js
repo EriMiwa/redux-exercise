62472171fec5d9b918f6688dcf28ab4bad194190
Erika Miwa <erikamiwa612@gmail.com>
2020-04-10 22:47:41 -0700

    add slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,44 +1,25 @@
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
-
-//actions
-export const bugAdded = description => ({
-  type: BUG_ADDED,
-  payload: {
-    description
-  }
-});
-
-export const bugResolved = id => ({
-  type: BUG_RESOLVED,
-  payload: {
-    description: "Bug1"
-  }
-});
+import { createSlice } from "@reduxjs/toolkit";
 
 //reducer
 let lastId = 0;
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducer: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolve: false
+      });
+    },
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case BUG_ADDED:
-      return [
-        ...state,
-        {
-          id: ++lastId,
-          description: action.payload.description,
-          resolve: false
-        }
-      ];
-  
-    case BUG_REMOVED:
-      return state.filter(bug => bug.id !== action.payload.id);
-
-    case BUG_RESOLVED:
-      return state.map(bug => bug.id !== action.payload.id ? bug : { ...bug});
-    
-    default:
-      return state;
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id)
+      bugs[index].resolved = true;
+    }
   }
-}
+});
+
+export const {bugAdded, bugResolved} = slice.actions;
+export default slice.reducer;
