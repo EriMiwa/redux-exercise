ac6dae27559175e6b68550e34bcdf2c8b7de3887
Erika Miwa <erikamiwa612@gmail.com>
2020-04-10 23:54:36 -0700

    add projects

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from './bugs';
+import reducer from './projects';
 
-export default function configureAppStore() {
+export default function() {
  return configureStore({ reducer });
 };
