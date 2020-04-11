7ef4673232569355795c870453b7d3eac008fe5c
Erika Miwa <erikamiwa612@gmail.com>
2020-04-11 14:25:41 -0700

    add bugAssignedToUser

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from 'reselect';
 
 //reducer
 let lastId = 0;
@@ -6,6 +7,12 @@ const slice = createSlice({
   name: 'bugs',
   initialState: [],
   reducers: {
+    bugAssignedToUser: (bugs, action) => {
+      const {bugId, userId} = action.payload;
+      const index = bugs.findIndex(bug => bug.id === bugId);
+      bugs[index].userId = userId;
+    },
+
     bugAdded: (bugs, action) => {
       bugs.push({
         id: ++lastId,
@@ -21,5 +28,18 @@ const slice = createSlice({
   }
 });
 
-export const {bugAdded, bugResolved} = slice.actions;
+export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
 export default slice.reducer;
+
+//Selector
+//Memoization
+export const getUnresolvedBugs = createSelector(
+  state => state.entities.bugs,
+  state => state.entities.projects,
+  bugs => bugs.filter(bug => !bug.resolved)
+)
+
+export const getBugsByUser = userId => createSelector(
+  state => state.entities.bugs,
+  bugs => bugs.filter(bug => bug.userId === userId)
+)
