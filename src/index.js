a4474059aa555cc419a733613b895cff41e19afb
Erika Miwa <erikamiwa612@gmail.com>
2020-04-11 17:44:42 -0700

    add func.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, bugAssignedToUser, getUnresolvedBugs, getBugsByUser} from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
+// import { bugAdded, bugResolved, bugAssignedToUser, getUnresolvedBugs, getBugsByUser} from './store/bugs';
+// import { projectAdded } from './store/projects';
+// import { userAdded } from './store/users';
+// import { createStore, applyMiddleware } from 'redux';
+// import reducer from './store/reducer';
+
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("Store changed!")
+store.dispatch((dispatch, getState) => {
+  dispatch({ type: 'bugsReceived', bugs: [1,2,3]})
+
 })
 
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-store.dispatch(bugResolved({ id: 1 }));
+// const store = configureStore();
 
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs)
+// store.subscribe(() => {
+//   console.log("Store changed!")
+// })
+
+// store.dispatch(userAdded({ name: "User 1" }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
+// store.dispatch(bugResolved({ id: 1 }));
+
+// const bugs = getBugsByUser(1)(store.getState());
+// console.log(bugs)
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
+import logger from './middleware/logger';
+import func from './middleware/func';
+
 
 export default function() {
- return configureStore({ reducer });
+  return configureStore({ 
+    reducer,
+    middleware: [
+      logger({ destination: 'console'}),
+      func
+    ]
+  });
 };
